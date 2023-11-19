1341f01f91e00ebd8976b655aa73112e81f4f7fb
Makoto098 <144616249+Makoto098@users.noreply.github.com>
2023-11-19 16:31:32 -0300

    Add files via upload

---
+++ copos.js
@@ -0,0 +1,39 @@
+var img = "";
+let status = "";
+
+function preload() {
+  img = loadImage("projeto_132_imagem_copos.jpeg");
+}
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecção de objetos";
+}
+function draw() {
+  image(img, 0, 0, 640, 420);
+  fill("#FF0000");
+  text("copo", 45, 75);
+  noFill();
+  stroke("#FF0000");
+  rect(30, 60, 450, 350);
+  text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+}
+function start() {
+  objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecção de objetos";
+}
+function modelLoaded() {
+  console.log("Model Loaded!");
+  Status = true;
+  objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+}
+function voltar() {
+  window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function copos() {
+  window.location = "copos.html";
+}
+
+function pessoas() {
+  window.location = "pessoas.html";
+}
+
+function mesa() {
+  window.location = "Mesa.html";
+}
+
+function Celulares() {
+  window.location = "Celulares.html";
+}
+
+function Cesta() {
+  window.location = "Cesta.html";
+}
